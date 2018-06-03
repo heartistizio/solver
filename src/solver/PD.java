2d733be13002f585cdf87c47716c654c6c5430bd
Tay <kjkosecki@gmail.com>
2018-06-03 18:28:12 +0200

    Merge branch 'master' of D:\PROGRAMMING\Repositories\solver with conflicts.

--- src/solver/PD.java
+++ src/solver/PD.java
@@ -8,7 +8,6 @@ public class PD {
     private List<Double> firstEquationFactors;
     private List<Double> secondEquationFactors;
     private List<Double> functionFactors;
-    private List<Point> CrossPointList = new ArrayList<>();
 
     public PD(List<Double> firstEquationFactors, List<Double> secondEquationFactors, List<Double> functionFactors) {
         this.firstEquationFactors = firstEquationFactors;
@@ -29,39 +28,21 @@ public class PD {
     }
 
     public List<Point> showCrossPoints(){
+        List<Point> PointList = new ArrayList<>();
         for(int i = 0; i < functionFactors.size(); i++){
             for(int n = 0; n < i; i++){
                 Double det = ((firstEquationFactors.get(i) * secondEquationFactors.get(n)) - (secondEquationFactors.get(i) * firstEquationFactors.get(n)));
                 Double x = ((((secondEquationFactors.get(n)) * functionFactors.get(i)) - (secondEquationFactors.get(i) * functionFactors.get(n))) / det);
                 Double y = ((((firstEquationFactors.get(i)) * functionFactors.get(n)) - (firstEquationFactors.get(n) * functionFactors.get(i))) / det);
-                CrossPointList.add(new Point (x, y));
+                PointList.add(new Point (x, y));
             }
         }
 
-        for(int i = 0; i < CrossPointList.size(); i++){
-            System.out.println("Point " + i);
-            CrossPointList.get(i).printPoint();
+        for(int i = 0; i < PointList.size(); i++){
+            PointList.get(i).printPoint();
 
         }
 
-        public Point V(){
-            Double minValue = Double.MAX_VALUE;
-            Point V = new Point(0.0, 0.0);
-            for(Point P : CrossPointList){
-                Double currentValue = firstEquationFactors.get(firstEquationFactors.size()) * P.getX() + secondEquationFactors.get(secondEquationFactors.size()) * P.getY();
-                if(currentValue < minValue) {
-                    minValue = currentValue;
-                    V.setX(P.getX());
-                    V.setY(P.getY());
-                }
-                return V;
-
-
-            }
-
-        }
-
-
-        return CrossPointList;
+        return PointList;
     }
 }
