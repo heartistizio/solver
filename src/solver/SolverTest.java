132c0ce191855bcd881afe333d51b456d24b5cbd
Krzysztof Kosecki <kjkosecki@gmail.com>
2018-06-04 13:34:23 +0200

    Update SolverTest.java

--- src/solver/SolverTest.java
+++ src/solver/SolverTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SolverTest {
 
-    Solver solver;
+    private Solver solver;
 
     @BeforeEach
     void preparation() {
@@ -15,15 +15,17 @@ class SolverTest {
     }
 
     @org.junit.jupiter.api.Test
-    void findCrossPointTest1() {
+    void findCrossPoint() {
         Line lineA = new Line(1,1, "<=");
         Line lineB = new Line(0,2, "<=");
 
         assertEquals(new Point(1.0,2.0), solver.findCrossPoint(lineA, lineB));
+
     }
 
     @org.junit.jupiter.api.Test
     void findCrossPoints() {
+
     }
 
     @org.junit.jupiter.api.Test
@@ -32,6 +34,19 @@ class SolverTest {
 
     @org.junit.jupiter.api.Test
     void isPointInRange() {
+
+        Line line = new Line(1,1, "<=");
+        Point point = new Point(0.0,0.0);
+        assertTrue(solver.isPointInRange(line.a, line.b, line.sign, point));
+
+        line = new Line(1,0, "<=");
+        point = new Point(0.0,0.0);
+        assertTrue(solver.isPointInRange(line.a, line.b, line.sign, point));
+
+        line = new Line(1,0, ">");
+        point = new Point(0.0,0.1);
+        assertTrue(solver.isPointInRange(line.a, line.b, line.sign, point));
+
     }
 
     @org.junit.jupiter.api.Test
