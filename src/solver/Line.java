49c1869908f1f5ff28f67e9c88c3be58d83e6ab9
Krzysztof Kosecki <kjkosecki@gmail.com>
2018-06-04 12:27:32 +0200

    Create Line.java

---
+++ src/solver/Line.java
@@ -0,0 +1,13 @@
+package solver;
+
+public class Line {
+
+    public final double x;
+    public final double y;
+    public final String sign;
+    public Line(double x, double y, String sign) {
+        this.x = x;
+        this.y = y;
+        this.sign = sign;
+    }
+}
